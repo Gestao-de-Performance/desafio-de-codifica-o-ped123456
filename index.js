81cf50ab088fc8df3b8cb952a1082e8a83dc64f4
ped123456 <guhss@hotmail.com>
2024-11-10 23:41:39 -0300

    Desafio de codificação
    Respostas dos exercicios 1, 2 e 3

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 var x = 5;
 var y = 7
+let numeros = [5, 10, 17, 20, 29, 35, 41, 50, 61, 70];
+let soma = 0
+for (let i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
+}
+
+let media = soma / numeros.length;
+
+console.log("A média dos números é:", media);
+
+function ehPrimos(numero) {
+    if (numero <= 1) return false;
+    for (let i = 2; i <=Math.sqrt(numero); i++) {
+        if (numero % i === 0) return false;
+           }
+        return true;
+}
+
+let primos = numeros.filter(ehPrimos);
+console.log("Números primos no vetor:", primos);
